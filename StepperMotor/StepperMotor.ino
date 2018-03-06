7e10f61540996da9e7bde010622a1b6f3fb2cc87
ragheedalhyder <ragheedalhyder@gmail.com>
2018-03-06 19:47:06 +0100

    added comments

--- StepperMotor/StepperMotor.ino
+++ StepperMotor/StepperMotor.ino
@@ -1,9 +1,9 @@
-int PUL=7; //define Pulse pin
+int PUL = 7; //define Pulse pin
 int DIR = 6; //define Direction pin
 int ENA = 5; //define Enable Pin
 #define DControl 8 //define Control Pin
 int Cont = digitalRead(DControl);
-int steps = 480;
+int steps = 480; //Number of steps turned
 void setup() {
 pinMode (PUL, OUTPUT);
 pinMode (DIR, OUTPUT);
@@ -12,16 +12,16 @@ pinMode (ENA, OUTPUT);
 
 void loop() {
   
-  Cont = digitalRead(DControl);
+  Cont = digitalRead(DControl); //Wait for control signal, could be replaced by interrupt
   while (Cont == HIGH) {
     Cont = digitalRead(DControl);   
     delay(1);
   }
   int j = 0;
-  while (Cont == LOW) //Forward 5000 steps
+  while (Cont == LOW) //Forward steps 
     {
       digitalWrite(DIR, LOW);
-      while (j < steps) //Backward 5000 steps
+      while (j < steps)
       {
       digitalWrite(PUL, HIGH);
       delayMicroseconds(100);
@@ -39,10 +39,10 @@ void loop() {
   }
   Cont = digitalRead(DControl);
   j = 0;
-  while (Cont == LOW) //Forward 5000 steps
+  while (Cont == LOW) //Backward steps
     {     
       digitalWrite(DIR, HIGH);
-      while (j < steps) //Backward 5000 steps
+      while (j < steps)
       {
       digitalWrite(PUL, HIGH);
       delayMicroseconds(100);
